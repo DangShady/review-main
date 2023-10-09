c9d6d5f3c2c770380021fa8b3270554f1a2bbba7
DangShady <dangshady1991@gmail.com>
2023-10-09 17:09:11 +0700

    profile

--- src/index.js
+++ src/index.js
@@ -3,57 +3,78 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const students = [
-  {
-    company: 'Alfreds Futterkiste',
-    contact: 'Maria Anders',
-    country: 'Germany'
-  },
-  {
-    company: 'Centro comercial Moctezuma',
-    contact: 'Francisco Chang',
-    country: 'Mexico'
-  },
-  {
-    company: 'Ernst Handel',
-    contact: 'Roland Mendel',
-    country: 'Austria'
-  },
-  {
-    company: 'Island Trading',
-    contact: 'Helen Bennett',
-    country: 'UK'
-  },
-  {
-    company: 'Laughing Bacchus Winecellars',
-    contact: 'Yoshi Tannamuri',
-    country: 'Canada'
-  },
-  {
-    company: 'Magazzini Alimentari Riuniti',
-    contact: 'Giovanni Rovelli',
-    country: 'Italy'
-  }
-]
+// const students = [
+//   {
+//     company: 'Alfreds Futterkiste',
+//     contact: 'Maria Anders',
+//     country: 'Germany'
+//   },
+//   {
+//     company: 'Centro comercial Moctezuma',
+//     contact: 'Francisco Chang',
+//     country: 'Mexico'
+//   },
+//   {
+//     company: 'Ernst Handel',
+//     contact: 'Roland Mendel',
+//     country: 'Austria'
+//   },
+//   {
+//     company: 'Island Trading',
+//     contact: 'Helen Bennett',
+//     country: 'UK'
+//   },
+//   {
+//     company: 'Laughing Bacchus Winecellars',
+//     contact: 'Yoshi Tannamuri',
+//     country: 'Canada'
+//   },
+//   {
+//     company: 'Magazzini Alimentari Riuniti',
+//     contact: 'Giovanni Rovelli',
+//     country: 'Italy'
+//   }
+// ]
 
-const jsx = (<table>
-  <thead>
-    <tr>
-      <th>Company</th>
-      <th>Contact</th>
-      <th>Country</th>
-    </tr>
-  </thead>
-  <tbody>
-    {students.map(student => (
-      <tr key={student.id}>
-        <td>{student.company}</td>
-        <td>{student.contact}</td>
-        <td>{student.country}</td>
-      </tr>
-    ))}
-  </tbody>
-</table>)
-
-root.render(jsx)
+// const jsx = (<table>
+//   <thead>
+//     <tr>
+//       <th>Company</th>
+//       <th>Contact</th>
+//       <th>Country</th>
+//     </tr>
+//   </thead>
+//   <tbody>
+//     {students.map(student => (
+//       <tr key={student.id}>
+//         <td>{student.company}</td>
+//         <td>{student.contact}</td>
+//         <td>{student.country}</td>
+//       </tr>
+//     ))}
+//   </tbody>
+// </table>)
+const Profile = (
+  <div className="container">
+    <div className="card">
+      <div className="card--header" />
+      <img
+        className="avatar"
+        src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        alt="avatar"
+      />
+      <div className="card--body">
+        <div>
+          <p className="text-header">Ruma Khan</p>
+          <p className="text-sub">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry
+          </p>
+          <button className="btn third">FOLLOW</button>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+root.render(Profile)
 
