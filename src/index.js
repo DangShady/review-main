c1418e9d966fd54b58d9b0fba4873a1af09f8c91
DangShady <dangshady1991@gmail.com>
2023-10-09 15:41:50 +0700

    fisrt commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const tick = (
+  root.render(
+    <div>
+      <h4>Browser's details: {navigator.userAgent}</h4>
+    </div>
+  ))
+  ;
+
