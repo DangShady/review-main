3a4d9a333b8d4ade499fe90174da9deec51e2c63
DangShady <dangshady1991@gmail.com>
2023-10-09 15:56:23 +0700

    jsx

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
 import ReactDOM from "react-dom/client";
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const tick = (
-  root.render(
-    <div>
-      <h4>Browser's details: {navigator.userAgent}</h4>
-    </div>
-  ))
-  ;
+const students = [
+  {
+    company: 'Alfreds Futterkiste',
+    contact: 'Maria Anders',
+    country: 'Germany'
+  },
+  {
+    company: 'Centro comercial Moctezuma',
+    contact: 'Francisco Chang',
+    country: 'Mexico'
+  },
+  {
+    company: 'Ernst Handel',
+    contact: 'Roland Mendel',
+    country: 'Austria'
+  },
+  {
+    company: 'Island Trading',
+    contact: 'Helen Bennett',
+    country: 'UK'
+  },
+  {
+    company: 'Laughing Bacchus Winecellars',
+    contact: 'Yoshi Tannamuri',
+    country: 'Canada'
+  },
+  {
+    company: 'Magazzini Alimentari Riuniti',
+    contact: 'Giovanni Rovelli',
+    country: 'Italy'
+  }
+]
+
+const jsx = (<table>
+  <thead>
+    <tr>
+      <th>Company</th>
+      <th>Contact</th>
+      <th>Country</th>
+    </tr>
+  </thead>
+  <tbody>
+    {students.map(student => (
+      <tr key={student.id}>
+        <td>{student.company}</td>
+        <td>{student.contact}</td>
+        <td>{student.country}</td>
+      </tr>
+    ))}
+  </tbody>
+</table>)
+
+root.render(jsx)
 
